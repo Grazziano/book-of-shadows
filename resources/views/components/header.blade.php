c25b8a8a87750f6dc0127693ca1bd5e8e5ac82ab
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-10-01 18:04:03 -0300

    feat: changing navbar items if user is logged in

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -8,11 +8,23 @@
                 <li><a href="#">Contos de Terror</a></li>
                 <li><a href="#">Crie sua Lenda</a></li>
                 <li><a href="#">Boletim Macabro</a></li>
+                @if(auth()->check())
+                <li><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                @endif
             </ul>
         </nav>
+        @if(auth()->check())
+        <div class="auth-buttons">
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit" class="btn btn-logout">Sair</button>
+            </form>
+        </div>
+        @else
         <div class="auth-buttons">
             <a href="{{ route('login') }}" class="btn btn-login">Login</a>
             <a href="{{ route('register') }}" class="btn btn-register">Registrar</a>
         </div>
+        @endif
     </div>
 </header>
