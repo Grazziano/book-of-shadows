4b65887758234b319aba12798a63c245b46fa768
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-10-01 16:05:19 -0300

    feat: creating home page

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container footer-content">
+        <div class="social-links">
+            <a href="#"><span>📱</span></a>
+            <a href="#"><span>📘</span></a>
+            <a href="#"><span>📸</span></a>
+        </div>
+        <p class="copyright">© 2025 Book of Shadows. Todos os direitos reservados.</p>
+    </div>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="container header-content">
+        <h1 class="logo flicker">Book of Shadows</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Lendas Urbanas</a></li>
+                <li><a href="#">Contos de Terror</a></li>
+                <li><a href="#">Crie sua Lenda</a></li>
+                <li><a href="#">Boletim Macabro</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
