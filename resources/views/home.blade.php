f3114c15b85fb7adc9df3d35853cb490e784ebf3
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-10-02 10:57:41 -0300

    feat: changing favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book of Shadows - Seu Grimório Digital</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
     <link href="https://fonts.googleapis.com/css2?family=Creepster&family=EB+Garamond:wght@400;600&display=swap" rel="stylesheet">
     <style>
         :root {
