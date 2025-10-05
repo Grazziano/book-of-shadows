9022baed82ae232434aecd7b61e0e574d3462b84
Grazziano Fagundes <grazzianofagundes@gmail.com>
2025-10-05 12:51:41 -0300

    feat: creating PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::with(['category', 'tags', 'user'])
+                   ->where('status', 'published')
+                   ->findOrFail($id);
+        
+        return view('posts.show', compact('post'));
+    }
+}
